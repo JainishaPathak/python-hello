3e55718f13ab79ad600df7118ed9f3e1f570dc32
JainishaPathak <jainisha.pathak@dmce.ac.in>
2023-02-12 09:14:39 +0530

    hello added

--- poem.py
+++ poem.py
@@ -3,3 +3,12 @@ print("hello world ")
 print("hello world ")
 print("hello world ")
 print("hello world ")
+
+print("hello world ")
+print("hello world ")
+print("hello world ")
+print("hello")
+
+
+
+
